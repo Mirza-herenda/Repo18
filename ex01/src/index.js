97f9cf7bab95315fad0c62ca881b1fafb9bf8d0c
Mirza <herendamirza87@gmail.com>
2022-04-15 03:47:33 +0200

    happy ending

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,26 +1,20 @@
+'use strict';
 
-let myArr=[];
-function *multiplication(x) 
-{
- for(let i =1;i<x;i++)   
- {
-     x=x*2;
-     yield x;
-     myArr.push(x);
- }
+let myArr = [];
+// Only change code below this line
+function* multiplication(x) {
+    for (let i = x; i > 0; i--) {
+        x *= 2;
+        yield x;
+        myArr.push(x)
+    }
+    return x;
 }
-const num=multiplication(3);
+var num = multiplication(3);
 
-for(let i =0;i<num;i++)
-{
-    console.log(num);
-  
+for (let i = 0; i < 3; i++) {
+    console.log(num.next().value);
 }
-console.log(num.next());
-console.log(num.next());
-console.log(num.next());
+// Only change code above this line
 
-
-
-
-
+module.exports = multiplication;
